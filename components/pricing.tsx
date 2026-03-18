aff9b88ee97ee23a3695da191ee77f615dc90405
DANVERSE <versedan13@gmail.com>
2026-03-17 20:21:36 -0700

    Add container to Pricing section for layout

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -8,6 +8,22 @@ export function Pricing() {
   return (
     <section id="pricing" className="text-white relative z-10 overflow-hidden bg-black">
       <PricingCards />
+      <div className="w-full">
+        <PricingExamplesStrip />
+      </div>
+    </section>
+  )
+}
+"use client"
+
+import React from "react"
+import { PricingExamplesStrip } from "./PricingExamplesStrip"
+import { PricingCards } from "./pricing-cards"
+
+export function Pricing() {
+  return (
+    <section id="pricing" className="text-white relative z-10 overflow-hidden bg-black">
+      <PricingCards />
       <div className="container mx-auto px-0 py-0 lg:py-0">
         <PricingExamplesStrip />
       </div>
